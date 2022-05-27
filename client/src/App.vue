8fb14d6afea6f84ace367aa9cafc5cc23b20b147
Wes Henderson <whenderson@nsgdv.com>
2022-05-27 05:12:24 -0500

    theme change

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
     <v-main>
-      <router-view/>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-}
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
